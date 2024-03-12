8ed37e64ab86e39df0b6561fe11554dcf5c4ef53
Komolika Kakrani <kanishkakakrani05@gmail.com>
2024-03-12 22:06:43 +0530

    Build card for projects

---
+++ src/components/Portfolio/Card.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import './Card.scss'
+
+function Card({ projectName, projectSubtitle, title, text }) {
+  return (
+    <div class="wrap animate pop">
+      <div class="overlay">
+        <div class="overlay-content animate slide-left delay-2">
+          <h1 class="pop-heading">{projectName}</h1>
+          <p
+            class="animate slide-left pop delay-5"
+            style={{
+              color: 'white',
+              marginBottom: '6rem',
+              marginLeft: '8px',
+              fontSize: '17px',
+            }}
+          >
+            {projectSubtitle}
+          </p>
+        </div>
+        <div class="image-content animate slide delay-5"></div>
+        <div class="dots animate">
+          <div class="dot animate slide-up delay-6"></div>
+          <div class="dot animate slide-up delay-7"></div>
+          <div class="dot animate slide-up delay-8"></div>
+        </div>
+      </div>
+      <div class="text-para">
+        <p>{title}</p>
+        <p>{text}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
+import Card from './Card'
 
 function Portfolio() {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -25,44 +26,19 @@ function Portfolio() {
             idx={15}
           />
         </h1>
-
-        <div class="wrap animate pop">
-          <div class="overlay">
-            <div class="overlay-content animate slide-left delay-2">
-              <h1 class="pop-heading">BiteBuddy</h1>
-              <p
-                class="animate slide-left pop delay-5"
-                style={{
-                  color: 'white',
-                   marginBottom: '6rem',
-                  marginLeft: '8px',
-                  fontSize: '17px',
-                }}
-              >
-                Food ordering app
-              </p>
-            </div>
-            <div class="image-content animate slide delay-5"></div>
-            <div class="dots animate">
-              <div class="dot animate slide-up delay-6"></div>
-              <div class="dot animate slide-up delay-7"></div>
-              <div class="dot animate slide-up delay-8"></div>
-            </div>
-          </div>
-          <div class="text-para">
-            <p>
-              BiteBuddy is a responsive and user-friendly food ordering web
-              application
-            </p>
-            <p>
-              Implemented state management using Redux for efficient handling of
-              dynamic data such as menu items, user cart, and order details.
-            </p>
-          </div>
-        </div>
+        <Card
+          projectName={'BiteBuddy'}
+          projectSubtitle={'Food ordering app'}
+          title={
+            ' BiteBuddy is a responsive and user-friendly food ordering web application'
+          }
+          text={
+            'Implemented state management using Redux for efficient handling of dynamic data such as menu items, user cart, and order details.'
+          }
+        />
       </div>
     </>
   )
 }
 
-export default Portfolio
+export default Portfolio 
